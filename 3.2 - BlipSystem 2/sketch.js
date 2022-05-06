607859bfb2ee0a550894cc0faa51fc3b58097fd0
unknown <konradfeher@outlook.com>
2022-05-06 12:48:31 +0200

    orbit color alpha

--- 3.2 - BlipSystem 2/sketch.js
+++ 3.2 - BlipSystem 2/sketch.js
@@ -102,7 +102,7 @@ class Blip{
             push();
                 noStroke();
                 for (var i = 0; i < this.orbit.length; i++) {
-                    fill(this.color, map(i, 0, this.orbit.length, 0, 255));
+                    fill(red(this.color), green(this.color), blue(this.color), map(i, 0, this.orbit.length, 0, 255));
                     circle(this.orbit[i].x, this.orbit[i].y, 3);
                 }
             fill(this.color);
