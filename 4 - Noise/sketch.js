c170610dd6dd65eb01761149a9cb541fa600d9ae
Fehér Konrád <h157451@stud.u-szeged.hu>
2023-01-20 05:19:12 +0100

    colors array for continuous lerping

--- 4 - Noise/sketch.js
+++ 4 - Noise/sketch.js
@@ -8,20 +8,26 @@ let time_step_slider;
 let step = 0.02;
 let time_step = 0.02;
 
-const canvas_size = 900
+let canvas_size = 900
 const mesh_size = 40
-let square_size = canvas_size / mesh_size
+let square_size;
+
+let colors = [0, 255]
 
 function setup() {
+    
+    square_size = floor(canvas_size / mesh_size)
+    canvas_size = square_size * mesh_size
+
     createCanvas(canvas_size, canvas_size)
     noFill()
     noStroke()
 
-    step_slider = createSlider(0, 0.2, step, 0);
+    step_slider = createSlider(0.001, 0.1, step, 0);
     step_slider.position(10, 10);
     step_slider.style('width', '100px');
 
-    time_step_slider = createSlider(0, 0.1, time_step, 0);
+    time_step_slider = createSlider(0, 0.05, time_step, 0);
     time_step_slider.position(width - 110, 10);
     time_step_slider.style('width', '100px');
 }
@@ -41,13 +47,20 @@ function draw() {
                 min_noise = min(timeNoise(i, j), min_noise)
             }
         }
+        max_noise *= 1.001
+        min_noise *= 0.999
     }
     for (let i = 0; i < mesh_size; i++) {
         for (let j = 0; j < mesh_size; j++) {
-            let rel_noise = map(timeNoise(i, j), min_noise, max_noise, 0, 1)
-            fill(rel_noise < 0.5 ? lerpColor(color("yellow"), color("white"), 2 * rel_noise) : lerpColor(color("white"), color("green"), 2*(rel_noise - .5)))
-            square(square_size * i, square_size * j, square_size)
+            let colorMap = map(timeNoise(i, j), min_noise, max_noise, 0, colors.length-1)
 
+            let fromColor = color(colors[floor(colorMap)])
+            let toColor = color(colors[floor(colorMap + 1)])
+
+            let fillColor = lerpColor(fromColor, toColor, colorMap % 1)
+
+            fill(fillColor)
+            square(square_size*i, square_size*j, square_size)
         }
     }
 
